2f36fbc79d665faf2c9a7f25377aa8caa429eea0
caherfe <caherfe@hotmail.com>
2014-01-30 12:00:22 +0100

    Separación de header y footer en php
    - Cuando empecemos a crear páginas a partir de ahora agradeceremos
    tener esto separado para si hay que hacer cambios hacerlos solo en un
    sitio =)
    - Añadida página en carpeta php como base para crear nuevas páginas

---
+++ DharmaPag/public_html/php/funciones/headerAndFooter.php
@@ -0,0 +1,37 @@
+<?php
+function cabecera(){
+	?>
+    <div id="cabecera">
+        <div id="logotipo"><img src="img/logo.jpg"></div>
+        <div id="menu">
+        <nav>
+            <ul>
+                <li><a href="index.html">Inicio</a></li>                        
+                <li><a href="#">Desplegable</a>
+                    <ul>
+                        <li><a href="#">Pares Magn&eacute;ticos</a></li>                        
+                        <li><a href="#">Acupuntura Emocional</a></li>
+                        <li><a href="#">Auriculoterapia</a></li>
+                        <li><a href="#">Acupuntura Abdominal</a></li>
+                        <li><a href="#">Homeopat&iacute;a</a></li>                        
+                        <li><a href="#">Quiromasaje</a></li>
+                        <li><a href="#">Drenaje Linf&aacute;tico</a></li>
+                        <li><a href="#">Osteopat&iacute;a Estructural</a></li>
+                        <li><a href="#">Reiki</a></li>
+                        <li><a href="#">Enfermeria</a></li>
+                        <li><a href="#">Facioterapia Vietnamita</a></li>
+                    </ul>
+                </li>
+                <li><a href="Contacto">Contacto</a></li>
+                <li><a href="Quienes">Quienes Somos</a></li>
+            </ul>
+        </nav> 
+        </div>
+    </div>
+	<?php
+}
+function pieDePagina(){
+	?>
+	<p> © 2014 Luis "el del Luissenger" Studios Copyright :P. All Rights Reserved.</p>	
+<?php }
+?>
